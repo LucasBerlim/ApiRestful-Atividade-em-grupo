239aef13bc5dd6aee34988183f1f426d069b24c0
LucasBerlim <berlimtere@gmail.com>
2024-10-16 14:50:24 -0300

    Initial commit

---
+++ src/main/java/org/serratec/ecommerce/pataMagica/PataMagicaApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.ecommerce.pataMagica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PataMagicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PataMagicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/serratec/ecommerce/pataMagica/PataMagicaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.ecommerce.pataMagica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PataMagicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
