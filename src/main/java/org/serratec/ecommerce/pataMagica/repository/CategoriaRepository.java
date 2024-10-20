50b4e15315432ab90b34613e7a8136eb310b8b36
eumichelems <michelemsilva28@gmail.com>
2024-10-20 19:48:02 -0300

    Update de imports com erro e Inclusão classes CategoriaController.java, CategoriaRepository.java e CategoriaService.java

---
+++ src/main/java/org/serratec/ecommerce/pataMagica/repository/CategoriaRepository.java
@@ -0,0 +1,8 @@
+package org.serratec.ecommerce.pataMagica.repository;
+
+import org.serratec.ecommerce.pataMagica.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
