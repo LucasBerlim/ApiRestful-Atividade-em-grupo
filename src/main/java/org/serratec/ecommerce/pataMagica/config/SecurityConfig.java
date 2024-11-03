28063b82c90c1d7f053d235d7401960e2b02b1ec
LucasBerlim <berlimtere@gmail.com>
2024-11-03 11:44:42 -0300

    Update security cors

--- src/main/java/org/serratec/ecommerce/pataMagica/config/SecurityConfig.java
+++ src/main/java/org/serratec/ecommerce/pataMagica/config/SecurityConfig.java
@@ -12,8 +12,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-@Configuration
+/*@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 	
@@ -23,8 +26,8 @@ public class SecurityConfig {
 		http.authorizeHttpRequests(authorize -> authorize
 		.requestMatchers(HttpMethod.GET, "**").permitAll()
 		//.requestMatchers(HttpMethod.PUT, "**").permitAll()
-		//.requestMatchers(HttpMethod.POST, "**").permitAll()
-		.requestMatchers(HttpMethod.POST, "**").hasRole("ADM")
+		.requestMatchers(HttpMethod.POST, "**").permitAll()
+		//.requestMatchers(HttpMethod.POST, "**").hasRole("ADM")
 		.requestMatchers(HttpMethod.DELETE, "**").hasRole("ADM")
 		.requestMatchers(HttpMethod.PUT, "**").hasRole("ADM"))
 		.csrf(csrf -> csrf.disable())
@@ -52,4 +55,56 @@ public class SecurityConfig {
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
 	}
+}*/
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @SuppressWarnings("removal")
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.GET, "**").permitAll()
+                .requestMatchers(HttpMethod.POST, "**").permitAll()
+                .requestMatchers(HttpMethod.POST, "**").hasRole("ADM")
+                .requestMatchers(HttpMethod.DELETE, "**").hasRole("ADM")
+                .requestMatchers(HttpMethod.PUT, "**").hasRole("ADM"))
+            .httpBasic(Customizer.withDefaults());
+        
+        return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager usuariosDetalhes() {
+        UserDetails usuario = User.builder()
+                .username("admin")
+                .password(encoder().encode("teste"))
+                .roles("ADM").build();
+        
+        UserDetails usuario2 = User.builder()
+                .username("rh")
+                .password(encoder().encode("teste"))
+                .roles("RH").build();
+        
+        return new InMemoryUserDetailsManager(usuario, usuario2);
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
